90550079a577a182c5508a3e29ede96b60972286
mdrasel101126 <mdrasel101126@gmail.com>
2022-09-02 22:36:45 +0600

    categories section added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log("hello");
+const loadNewsCategories = async () => {
+  const url = "https://openapi.programming-hero.com/api/news/categories";
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayNewsCategories(data.data.news_category);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayNewsCategories = (categories) => {
+  console.log(categories);
+  const categoriesContainer = document.getElementById("categories-container");
+  categories.forEach((category) => {
+    const categoryDiv = document.createElement("div");
+    categoryDiv.classList.add("mx-3");
+    categoryDiv.classList.add("pointer-cursor");
+    categoryDiv.innerHTML = `
+       <p onclick="loadNews('${category.category_id}')">${category.category_name}</p>
+    `;
+    categoriesContainer.appendChild(categoryDiv);
+  });
+};
+
+const loadNews = async (categoryId) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+loadNewsCategories();
